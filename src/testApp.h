fc768a2c069909fbe4dc0e5b5487cdf693d933d2
Yasuhiko Nishimura <yasuhiko.nishimura@gmail.com>
2014-02-05 03:05:22 +0900

    first commit

---
+++ src/testApp.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "ofMain.h"
+
+class testApp : public ofBaseApp{
+    //--------------------------------------------------------------
+    void setup(){
+        ofBackground(0);
+        ofSetFrameRate(60);
+        ofSetVerticalSync(true);
+    }
+    
+    //--------------------------------------------------------------
+    void update(){
+        
+    }
+    
+    //--------------------------------------------------------------
+    void draw(){
+    }
+};
+
