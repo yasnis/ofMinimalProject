b8261c675d8f7bbd205a58c616d4be5060224af5
Yasuhiko Nishimura <yasuhiko.nishimura@gmail.com>
2014-02-07 02:58:23 +0900

    remove testApp.h

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,26 @@
 #include "ofMain.h"
-#include "testApp.h"
+
+class testApp : public ofBaseApp{
+    //--------------------------------------------------------------
+    void setup(){
+        ofBackground(0);
+        ofSetFrameRate(60);
+        ofSetVerticalSync(true);
+    }
+    
+    //--------------------------------------------------------------
+    void update(){
+        
+    }
+    
+    //--------------------------------------------------------------
+    void draw(){
+        
+    }
+};
 
 //========================================================================
 int main( ){
-
-	ofSetupOpenGL(960,540, OF_WINDOW);			// <-------- setup the GL context
-
-	// this kicks off the running of my app
-	// can be OF_WINDOW or OF_FULLSCREEN
-	// pass in width and height too:
+	ofSetupOpenGL(960,540, OF_WINDOW);
 	ofRunApp( new testApp());
-
 }
